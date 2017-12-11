e0b5ccce56ad5c17526a3ab6682a233dea198bfe
msrocka <michael.srocka@gmail.com>
2017-12-11 15:28:34 +0100

    file utils

--- io.go
+++ io.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 // WriteFile writes the given EcoSpold data set to the file with the given path.
@@ -13,4 +14,38 @@ func WriteFile(es *EcoSpold, filePath string) error {
 		return err
 	}
 	return ioutil.WriteFile(filePath, data, os.ModePerm)
+}
+
+// ReadFile reads an EcoSpold model from the given XML file.
+func ReadFile(path string) (*EcoSpold, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var es EcoSpold
+	if err := xml.Unmarshal(data, &es); err != nil {
+		return nil, err
+	}
+	return &es, err
+}
+
+// EachFile processes each EcoSpold file in the given folder and calls the given
+// function until there is no more file in the folder or the given function
+// returns false.
+func EachFile(dir string, fn func(spold *EcoSpold) bool) error {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		path := filepath.Join(dir, f.Name())
+		spold, err := ReadFile(path)
+		if err != nil {
+			return err
+		}
+		if !fn(spold) {
+			break
+		}
+	}
+	return nil
 }
